8500e841f0578bcb06490a9a7ddd5bf8c7b2d498
DineshPatil025 <dineshbpatil74@gmail.com>
2025-08-22 20:23:00 +0530

    added email control

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -4,14 +4,15 @@ export interface IStudent extends Document {
   firstName: string;
   lastName: string;
   mobileNumber?: number;
+  email?: string;
 
 }
 
 const StudentSchema: Schema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  mobileNumber: { type: Number, required: false }
-
+  mobileNumber: { type: Number, required: false },
+  email: { type: String, required: false }
 });
 
 export default mongoose.model<IStudent>('Student', StudentSchema);
